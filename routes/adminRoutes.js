0ab51925ec30f8daaafd047e29c2dcbf717229ff
Malika Sharma <malika.dogra2297@gmail.com>
2021-05-08 00:24:19 +0530

    second commit

---
+++ routes/adminRoutes.js
@@ -0,0 +1,8 @@
+const path = require('path');//path module
+const router = require('express').Router();//a new router object created to handle requests.
+
+router.get('/shophome',(req , res , next) =>//working for the link shophome inside the middleware
+{
+    res.send("<h1 style='color:pink;text-align:center;'>This is the homepage</h1>");
+})
+module.exports = router;
--- shopserver.js
+++ shopserver.js
@@ -1,8 +1,10 @@
+const path = require('path');//requiring path module to work with directory path and file paths.
 const express = require("express");//requiring express module server
 const app = express();//making call to the express server
+const adminRoutes = require('./routes/adminRoutes');
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminRoutes);//middlewares in between
 
-app.use(userRoutes);
+// app.use(userRoutes);//middleware
 
 app.listen(3000);
