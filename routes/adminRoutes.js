a8b01990b526dc2288c46720e0fce8c1edd18d5b
Malika Sharma <malika.dogra2297@gmail.com>
2021-05-08 15:51:21 +0530

    second commit

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,6 @@ const router = require('express').Router();//a new router object created to hand
 
 router.get('/shophome',(req , res , next) =>//working for the link shophome inside the middleware
 {
-    res.send("<h1 style='color:pink;text-align:center;'>This is the homepage</h1>");
-})
-module.exports = router;
+    res.send("<h1 style='color:purple;text-align:center;'>This is the homepage</h1>");
+});
+module.exports =  router;
